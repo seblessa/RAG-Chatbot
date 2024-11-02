07734d764f806f740d6d01f5347622e3f37c72ba
seblessa <sebastiaolessa2002@outlook.com>
2024-11-02 15:04:44 +0000

    update

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, request, render_template,jsonify,stream_with_context,Response
+from flask import Flask, request, render_template, jsonify, stream_with_context, Response
 from haystack_components.pipeline import answer_question, document_processor_pipeline
 import os
 from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
-messages=[]
+messages = []
+
+
 @app.route('/', methods=['GET'])
 def index():
-    messages=[]
+    messages = []
     return render_template('index.html', messages=messages)
+
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
@@ -18,20 +22,23 @@ def send_message():
         return jsonify({'status': 'received'})
     return jsonify({'status': 'error'}), 400
 
+
 @app.route('/stream_response', methods=['POST'])
 def stream_response():
     message = request.json.get('message', '')
     if not message:
         return jsonify({'error': 'No message provided'}), 400
-    answer=answer_question(message)['LLM']['response']
+    answer = answer_question(message)['LLM']['response']
     messages.append(('receiver', answer))
     return answer
+
 
 # Configuração para armazenamento temporário de ficheiros
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
+
 
 @app.route('/upload_file', methods=['POST'])
 def upload_file():
@@ -56,7 +63,7 @@ def upload_file():
     except Exception as e:
         print(e)
         return jsonify({'status': 'error', 'message': str(e)}), 500
-    
+
 
 if __name__ == '__main__':
     app.run(debug=False, threaded=False)
